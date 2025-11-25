f641dbee1b292b9fbf006297ff6a088a0ee1f841
Ariel Galvez <ari.galvez@duocuc.cl>
2025-11-25 00:08:20 -0300

    Dockerizado

--- demo/src/main/java/com/example/demo/lab/controller/LaboratorioController.java
+++ demo/src/main/java/com/example/demo/lab/controller/LaboratorioController.java
@@ -23,6 +23,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/laboratorios")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200") // 👈 AGREGA ESTO
 public class LaboratorioController {
 
     private final LaboratorioService service;
